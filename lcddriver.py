be38fc0fb2616208d4e6fb06d70e2209d1c88622
Rafael Cruz <rafaelcruz@outlook.com>
2017-08-19 04:31:02 +0200

    Setando BUS para 0, funciona em 20x4

--- lcddriver.py
+++ lcddriver.py
@@ -64,7 +64,7 @@ Rs = 0b00000001 # Register select bit
 class lcd:
    #initializes objects and lcd
    def __init__(self):
-      self.lcd_device = i2c_lib.i2c_device(ADDRESS)
+      self.lcd_device = i2c_lib.i2c_device(ADDRESS,0)
 
       self.lcd_write(0x03)
       self.lcd_write(0x03)
